e522a8dd7b91c0afaadad11516992e63b8cff00f
Jainer <jainer.bonilla00@usc.edu.co>
2026-05-03 14:37:30 -0500

    feat: Mejorar experiencia móvil PWA e instalar app

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,10 +1,16 @@
 const CACHE = "financiera-np-v1";
+const OFFLINE_URL = "/";
+const ASSETS = [
+  "/",
+  "/static/manifest.json",
+  "/static/logo.png",
+  "/static/icons/icon.svg",
+  "/static/service-worker.js"
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) =>
-      cache.addAll(["/", "/static/manifest.json", "/static/icons/icon.svg"]).catch(() => {})
-    )
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).catch(() => {})
   );
   self.skipWaiting();
 });
@@ -22,7 +28,19 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   const url = new URL(event.request.url);
   if (url.origin !== self.location.origin) return;
+
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          if (response && response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => caches.match(OFFLINE_URL));
+    })
   );
 });
